e02ba60bb754f583b307f8dd95ef6ff5b7f434f5
Ishant Bhurani <ishantbhurani@gmail.com>
2021-09-10 23:07:56 +0530

    Added functionality to toggle share overlay on mobile/tablet

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const shareBtn = document.querySelector("#share-btn");
 const overlay = document.querySelector(".share-overlay");
+const overlayShareBtn = document.querySelector("#overlay-share-btn");
 
 shareBtn.onclick = (e) => {
   shareBtn.classList.toggle("active");
   overlay.classList.toggle("active");
+  shareBtn.blur();
+};
+
+overlayShareBtn.onclick = (e) => {
+  shareBtn.classList.remove("active");
+  overlay.classList.remove("active");
   shareBtn.blur();
 };
